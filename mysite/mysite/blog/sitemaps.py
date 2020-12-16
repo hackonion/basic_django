211067ebe8b90eda6989aab97f238244ffcf3d7a
hackonion <jorge21913@hotmail.com>
2020-12-16 17:11:23 -0600

    Implementing custom queries,rss feed,sitemap

---
+++ mysite/mysite/blog/sitemaps.py
@@ -0,0 +1,14 @@
+from django.contrib import sitemaps
+from django.contrib.sitemaps import Sitemap
+from .models import Post
+
+class PostSitmap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.9
+    
+    def items(self):
+        return Post.published.all()
+    
+    def lastmod(self, obj):
+        return obj.updated
+    
---
+++ mysite/mysite/blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from .feeds import LastPostsFeed
+
+app_name = 'blog'
+
+urlpatterns = [
+    # post views
+    path('', views.post_list, name='post_list'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    # path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_shared, name='post_share'),
+    path('feeds/', LastPostsFeed(), name='post_feed'),
+    
+]
