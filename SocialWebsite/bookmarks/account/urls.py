f6d73f483ee8a34277c99299e305d18e3d3574f8
hackonion <jorge21913@hotmail.com>
2020-12-25 21:57:32 -0600

    Add register form

--- SocialWebsite/bookmarks/account/urls.py
+++ SocialWebsite/bookmarks/account/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
-    
+    path('register/', views.register, name='register')
     
     
 ]
