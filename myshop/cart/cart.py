ec5c65ab0dea54bbc55e19e56e98a3bb05566c3d
hackonion <jorge21913@hotmail.com>
2021-01-09 20:48:24 -0600

    Adding products to the cart

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -1,31 +1,28 @@
 from decimal import Decimal
-from typing import ItemsView
 from django.conf import settings
 from shop.models import Product
 
 class Cart(object):
-    
     def __init__(self, request):
         """
-            Init cart
+        Initialize the cart.
         """
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
-            # Save an empty cart in the session
+        # save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
         
-    def add(self, product, quantity=1, overrride_quantity=False):
+    def add(self, product, quantity=1, override_quantity=False):
         """
-            Add a product to the cart or update its quantity.
+        Add a product to the cart or update its quantity.
         """
         product_id = str(product.id)
-        if product_id  not in self.card:
-            self.cart[product_id] = {'quantity':0,
-                                     'price':str(product.price)}
-            
-        if overrride_quantity:
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0,
+                                     'price': str(product.price)}
+        if override_quantity:
             self.cart[product_id]['quantity'] = quantity
         else:
             self.cart[product_id]['quantity'] += quantity
@@ -70,12 +67,11 @@ class Cart(object):
         return sum(item['quantity'] for item in self.cart.values())
     
     def get_total_price(self):
-        
-        return sum(Decimal(item['price'] * item['quantity'] for item in self.cart.values()))
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
     
     def clear(self):
         # Remove cart session 
-        del self.session(settings.CART_SESSION_ID)
+        del self.session[settings.CART_SESSION_ID]
         self.save()
     
     
--- myshop/shop/views/Product.py
+++ myshop/shop/views/Product.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from cart.forms import CartAddProductForm
 from shop.models import Product, Category
 
 def product_list(request, category_slug=None):
@@ -22,9 +23,11 @@ def product_detail(request, id, slug):
                                 id=id,
                                 slug=slug,
                                 available=True)
+    cart_product_form = CartAddProductForm()
     return render(request,
                   'shop/product/detail.html',
-                  {'product': product})
+                  {'product': product,
+                   'cart_product_form':cart_product_form})
     
     
 
